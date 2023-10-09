b9e2373fc38bbf4fd2cfa919365c11da5230596f
chb816 <sfr9802@daum.net>
2023-10-10 00:58:18 +0900

    update & dispose repo

--- stock market/yf_df.py
+++ stock market/yf_df.py
@@ -1,13 +1,7 @@
-import numpy as np
 import pandas as pd
 import yfinance as yf
-import plotly.graph_objs as go
-import plotly.express as px
-import matplotlib.pyplot as plt
-import plotly.express as px
-from plotly.subplots import make_subplots
-
 def yf_to_df(tker : str, per : str, start_date : str, end_date : str) :
+
     data = yf.download(tickers = tker, interval = per, start=start_date, end = end_date)
     df_stock = pd.DataFrame({'High' : data['High'], 
                                 'Low' : data['Low'], 
@@ -15,4 +9,5 @@ def yf_to_df(tker : str, per : str, start_date : str, end_date : str) :
                                 'Open' : data['Open'],
                                 'Adj Close' : data['Adj Close']},
                                 index = data.index)
+    df_stock.to_csv(''+tker+'_'+start_date+'.csv', sep=',', float_format='%.64f')
     return df_stock
